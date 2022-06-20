305aaeeb12b6573bfb413f6414863979c28e2cca
Adam Majer <majer.ad.2019@skola.ssps.cz>
2022-06-20 15:31:04 +0200

    first commit

---
+++ Counting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KvadrFunkce
+{
+    public class Counting
+    {
+        public static double Discriminant(double a, double b, double c)
+        {
+            return b * b - 4.0 * a * c;
+        }
+        public static double AnswerDbigernullX1(double D, double a ,double b)
+        {            
+            D = Math.Sqrt(D) / (2 * a); 
+            b = -b / (2 * a);
+            return Math.Round(b + D,3);
+        }
+        public static double AnswerDbigernullX2(double D, double a, double b)
+        {
+            D = Math.Sqrt(D) / (2 * a);
+            b = -b / (2 * a);
+            return Math.Round(b - D, 3);
+        }
+        public static string AnswerDSmallernullResoltX1(double D, double a, double b)
+        {
+            D = Math.Abs(D);
+            D = Math.Round(Math.Sqrt(D)/ (2 * a),3);
+            b = Math.Round(-b / (2 * a),3);      
+            return  $"{b} + {D}i" ;
+        }
+        public static string AnswerDSmallernullResoltX2(double D, double a, double b)
+        {
+            D = Math.Abs(D);
+            D = Math.Round(Math.Sqrt(D) / (2 * a), 3);
+            b = Math.Round(-b / (2 * a),3);
+            return $"{b} - {D}i";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KvadrFunkce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                
+                string x1 = "";
+                string x2 = "";
+                Console.Write("a = ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                if (a != 0)
+                {
+                    Console.Write("b = ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("c = ");
+                    double c = Convert.ToDouble(Console.ReadLine());
+
+                    double d = Counting.Discriminant(a, b, c);
+                    if (d >= 0)
+                    {
+                        x1 = $"{Counting.AnswerDbigernullX1(d, a, b)}";
+                        x2 = $"{Counting.AnswerDbigernullX2(d, a, b)}";
+                    }
+                    else
+                    {
+                        x1 = Counting.AnswerDSmallernullResoltX1(d, a, b);
+                        x2 = Counting.AnswerDSmallernullResoltX2(d, a, b);
+                    }
+
+                    Console.WriteLine($"x1 = {x1}");
+                    Console.WriteLine($"x2 = {x2}");
+
+                }
+                else
+                {
+                    Console.WriteLine("pokud se A = 0, není kvadratická rovnice");
+                }
+            }
+            
+
+        }
+    }
+}
